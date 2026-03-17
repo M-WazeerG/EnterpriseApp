d3c2b997e228ccf8fdd7b3ed2627afdbfc993b86
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-03-17 14:16:54 +0200

    Closes #15 Create Boat Factory

---
+++ src/main/java/za/ac/cput/factory/BoatFactory.java
@@ -0,0 +1,35 @@
+/*
+Author: Mogamat Yaseen Kannemeyer
+Student No: 240453182
+*/
+
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Boat;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+
+public class BoatFactory {
+
+    public static Boat createBoat(String boatNumber, String boatName, int capacity, LocalDate tripDate) {
+        if (Helper.isNullOrEmpty(boatNumber) || Helper.isNullOrEmpty(boatName)) {
+            return null;
+        }
+
+        if (capacity <= 0) {
+            return null;
+        }
+
+        if (tripDate == null) {
+            return null;
+        }
+
+        return new Boat.Builder()
+                .setBoatNumber(boatNumber)
+                .setBoatName(boatName)
+                .setCapacity(capacity)
+                .setTripDate(tripDate)
+                .build();
+    }
+}
